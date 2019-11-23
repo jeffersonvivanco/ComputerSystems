5b4e8777396a69c2da689a6caec7b3e40768d5db
Jefferson Vivanco <jv1019@nyu.edu>
2019-11-22 19:24:52 -0500

    adding notes

---
+++ cmake-build-debug/CMakeFiles/problem3.5.c.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/problem3.5.c.dir/problem3.5.c.o"
+  "problem3.5.c.pdb"
+  "problem3.5.c"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/problem3.5.c.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
